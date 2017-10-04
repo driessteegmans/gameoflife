ac738946f4d1a1f7fd962425569fb31647c43ba8
Daan Vankerkom <daan@vankerkom.be>
2017-10-04 21:17:36 +0200

    Refactor render, add seed button and fix projection
    Refactored the render code by using a list.
    Added seed method to which can be triggered by pressing the V button.
    Changed clear method by creating a new array on reset.

---
+++ Game/LifeGame/Cell.cs
@@ -0,0 +1,8 @@
+﻿namespace LifeGame
+{
+    public struct Cell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
--- Game/LifeGame/Game.cs
+++ Game/LifeGame/Game.cs
@@ -12,6 +12,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using OpenTK;
@@ -38,25 +39,25 @@ namespace LifeGame
         private readonly Stopwatch _frameRenderStopwatch = new Stopwatch();
         private readonly Stopwatch _simulationStopwatch = new Stopwatch();
 #endif
+        private readonly Random _random = new Random();
 
-        private Matrix4 matrix = Matrix4.CreateTranslation(0, 0, 0);
+        private Matrix4 _projectionMatrix = Matrix4.Identity;
+
+        private Vector2 _mousePosition = new Vector2(0, 0);
         private Vector2 MoCinitialvec = new Vector2(0f, 0f);
-        private Vector2 MoCdvec = new Vector2(0f, 0f);
         private Vector3 PrevViewpoint = new Vector3(0f, 0f, 0f);
 
-        //variables
-
         // Camera
-        private Vector3 ViewPointV = new Vector3(-1f, -1f, 0f);
-        private float ZoomMulti = 0.2f;
+        private Vector3 _moveTranslation = new Vector3(Vector3.Zero);
 
         // Game State
         private bool _paused = true;
 
         // Grid Information
 
-        private readonly byte[,] _aliveGrid = new byte[GridSize, GridSize];
+        private byte[,] _aliveGrid = new byte[GridSize, GridSize];
         private readonly byte[,] _neighboursGrid = new byte[GridSize, GridSize];
+        private readonly List<Cell> _aliveList = new List<Cell>();
 
         #endregion
 
@@ -71,55 +72,67 @@ namespace LifeGame
             VSync = VSyncMode.On;
 
             // Window Resize
-            Resize += (sender, e) => { GL.Viewport(0, 0, Width, Height); };
+            Resize += (sender, e) =>
+            {
+                Console.WriteLine("Window resize.");
+
+                _projectionMatrix = Matrix4.CreateOrthographicOffCenter(0, Width, Height, 0, 0, 1);
+
+                GL.Viewport(0, 0, Width, Height);
+            };
 
             // Mouse Click Events
             Mouse.ButtonDown += (sender, e) =>
             {
-                MoCinitialvec = MousePosition(e.X, e.Y, this);
-                MoCdvec = MoCinitialvec;
+                MoCinitialvec = MousePosition(e.X, e.Y);
+                _mousePosition = MoCinitialvec;
 
 
                 //start moving viewpoint
                 if (e.Button == MouseButton.Right)
                 {
-                    PrevViewpoint = ViewPointV;
+                    PrevViewpoint = _moveTranslation;
                 }
             };
 
             // Mouse wheel scrolling - zoom in / outscroll wheel - zoom in / out
             Mouse.WheelChanged += (sender, e) =>
             {
-                if (ZoomMulti - e.DeltaPrecise * 0.001f > 0)
+                /*if (ZoomMulti - e.DeltaPrecise * 0.001f > 0)
                 {
                     ZoomMulti -= e.DeltaPrecise * 0.001f;
-                }
+                }*/
             };
 
             // Mouse moving
             Mouse.Move += (sender, e) =>
             {
                 //for adding object
-                MoCdvec = MousePosition(e.X, e.Y, this);
+                _mousePosition = MousePosition(e.X, e.Y);
 
                 if (e.Mouse.LeftButton == ButtonState.Pressed)
                 {
+                    var mouseX = (int)_mousePosition.X;
+                    var mouseY = (int)_mousePosition.Y;
+
                     // TODO(BERKAN) EXPENSIVE !!! TRY - CATCH = EXPENSIVE!!
                     try
                     {
-                        _aliveGrid[(int)MoCdvec.X, (int)MoCdvec.Y] = 1;
+                        if (_aliveGrid[mouseX, mouseY] != 1)
+                        {
+                            CreateCell(mouseX, mouseY);
+                        }
                     }
                     catch
                     {
                     }
                 }
 
-
                 //for moving viewpoint
                 if (e.Mouse.RightButton == ButtonState.Pressed)
                 {
-                    ViewPointV = new Vector3((MoCdvec - MoCinitialvec).X / (Width * ZoomMulti),
-                                     (MoCdvec - MoCinitialvec).Y / (Height * ZoomMulti), 0) + PrevViewpoint;
+                    _moveTranslation = new Vector3((_mousePosition - MoCinitialvec).X / (Width),
+                                     (_mousePosition - MoCinitialvec).Y / (Height), 0) + PrevViewpoint;
                 }
             };
 
@@ -140,18 +153,18 @@ namespace LifeGame
                             _paused = !_paused;
                             break;
                         }
-                        
+
                     case 'c': // Clears the screen.
                         {
                             // Fill arrays with zeroes.
-                            for (var i = 0; i < GridSize; i++)
-                            {
-                                for (var z = 0; z < GridSize; z++)
-                                {
-                                    _aliveGrid[i, z] = 0;
-                                }
-                            }
+                            ClearGrid();
+                            break;
+                        }
 
+
+                    case 'v':
+                        {
+                            SeedGrid();
                             break;
                         }
 
@@ -163,16 +176,6 @@ namespace LifeGame
 
             // Rendering
             RenderFrame += Game_RenderFrame;
-
-            // Seed grid array
-            var rand = new Random();
-            for (var i = 200; i < GridSize - 200; i++)
-            {
-                for (var z = 200; z < GridSize - 200; z++)
-                {
-                    _aliveGrid[i, z] = (byte)rand.Next(3);
-                }
-            }
         }
 
         #endregion
@@ -187,32 +190,32 @@ namespace LifeGame
             // Move left or right.
             if (Keyboard[Key.A])
             {
-                ViewPointV.X += 0.01f;
+                _moveTranslation.X += 0.01f;
             }
             else if (Keyboard[Key.D])
             {
-                ViewPointV.X -= 0.01f;
+                _moveTranslation.X -= 0.01f;
             }
 
             // Move up or down.
             if (Keyboard[Key.W])
             {
-                ViewPointV.Y -= 0.01f;
+                _moveTranslation.Y -= 0.01f;
             }
             else if (Keyboard[Key.S])
             {
-                ViewPointV.Y += 0.01f;
+                _moveTranslation.Y += 0.01f;
             }
 
             // Zoom-in or out.
-            if (Keyboard[Key.Z])
+            /*if (Keyboard[Key.Z])
             {
                 ZoomMulti += 0.0001f;
             }
             else if (Keyboard[Key.X] && ZoomMulti > 0.001f)
             {
                 ZoomMulti -= 0.0001f;
-            }
+            }*/
 
             // Do not run the simulations if the game is paused.
             if (_paused)
@@ -222,7 +225,7 @@ namespace LifeGame
 #if DEBUG
             _simulationStopwatch.Restart();
 #endif
-            
+
             //calculate neighbours
             for (int x = 0; x < GridSize; x++)
             {
@@ -279,16 +282,19 @@ namespace LifeGame
                 }
             }
 
+            // Clear alive list.
+            _aliveList.Clear();
+
             //kill / revive
-            for (int x = 0; x < GridSize; x++)
+            for (var x = 0; x < GridSize; x++)
             {
-                for (int y = 0; y < GridSize; y++)
+                for (var y = 0; y < GridSize; y++)
                 {
                     // Swaped order to reduce calls.
                     // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                     if (_neighboursGrid[x, y] == 3)
                     {
-                        _aliveGrid[x, y] = 1;
+                        CreateCell(x, y);
                     }
                     else if (_neighboursGrid[x, y] < 2 || _neighboursGrid[x, y] > 3)
                     {
@@ -305,6 +311,17 @@ namespace LifeGame
 #endif
         }
 
+        private void CreateCell(int x, int y)
+        {
+            _aliveGrid[x, y] = 1;
+
+            _aliveList.Add(new Cell
+            {
+                X = x,
+                Y = y
+            });
+        }
+
         private void Game_RenderFrame(object sender, FrameEventArgs e)
         {
 
@@ -313,42 +330,26 @@ namespace LifeGame
 #endif
 
             // Clear OpenGL window buffer.
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
             GL.MatrixMode(MatrixMode.Projection);
-
-            matrix = Matrix4.CreateTranslation(ViewPointV);
-
-            GL.LoadMatrix(ref matrix);
-            GL.Ortho(-Width * ZoomMulti, Width * ZoomMulti, -Height * ZoomMulti,
-                Height * ZoomMulti, 0.0, 4.0);
+            GL.LoadMatrix(ref _projectionMatrix);
 
             // Render graphics
-            // Now draw the objects to the gamewindow
 
             GL.Color3(Color.Yellow);
-
-            GL.Begin(PrimitiveType.Quads);
+            GL.Begin(PrimitiveType.Points);
 
             // Draw the cells.
-            for (int x = 0; x < GridSize; x++)
+            foreach (var cell in _aliveList)
             {
-                for (int y = 0; y < GridSize; y++)
-                {
-                    // Check if a cell is alife.
-                    if (_aliveGrid[x, y] == 1)
-                    {
-                        GL.Vertex2(x, y);
-                        GL.Vertex2(x, y + 1);
-                        GL.Vertex2(x + 1, y + 1);
-                        GL.Vertex2(x + 1, y);
-                    }
-                }
+                GL.Vertex2(cell.X, cell.Y);
+                // GL.Vertex2(cell.X, cell.Y + 1);
+                // GL.Vertex2(cell.X + 1, cell.Y + 1);
+                // GL.Vertex2(cell.X + 1, cell.Y);
             }
 
             GL.End();
-
-            // Draw a circle at the viewpoint.
-            DrawCircle(30, ViewPointV.X, ViewPointV.Y, 1);
 
             // OpenGL Swap the window buffers to display the new frame.
             SwapBuffers();
@@ -361,31 +362,47 @@ namespace LifeGame
             Title = string.Format(WindowTitle, Math.Round(RenderFrequency, 4));
         }
 
-        //draw a circle on the opentk gamewindow
-        public void DrawCircle(int segments, float xpos, float ypos, float radius)
+        /// <summary>
+        /// Get the mouse position as a vector scaled to the zoom.
+        /// </summary>
+        /// <param name="mouseX"></param>
+        /// <param name="mouseY"></param>
+        /// <returns></returns>
+        public Vector2 MousePosition(float mouseX, float mouseY)
         {
-            GL.Begin(PrimitiveType.Polygon);
+            // TODO Implement zoom.
 
-            for (int i = 0; i < segments; i++)
-            {
-                float theta = (2.0f * (float)Math.PI * i) / (float)segments;
-                float cxx = radius * (float)Math.Cos(theta);
-                float cyy = radius * (float)Math.Sin(theta);
-                GL.Vertex2(xpos + cxx, ypos + cyy);
-            }
-
-            GL.End();
+            return new Vector2(mouseX, mouseY);
         }
 
-
-        //get the position of the mouse as a vector scaled to the gamewindow
-        public Vector2 MousePosition(float mX, float mY, GameWindow game)
+        private void ClearGrid()
         {
-            Vector2 vecMousePos = new Vector2(((mX) / (float)Width - 0.5f) * Width * 2 *
-                                              ZoomMulti - ViewPointV.X * Width * ZoomMulti,
-                0 - ((mY) / (float)Height - 0.5f)
-                * Height * 2 * ZoomMulti - ViewPointV.Y * Height * ZoomMulti);
-            return vecMousePos;
+            _aliveGrid = new byte[GridSize, GridSize];
+            _aliveList.Clear();
+        }
+
+        private void SeedGrid()
+        {
+            // Clear the old grid.
+            ClearGrid();
+
+            // Seed the grid with random data.
+            for (var x = 0; x < GridSize; x++)
+            {
+                for (var y = 0; y < GridSize; y++)
+                {
+                    var value = (byte)_random.Next(3);
+
+                    if (value == 1)
+                    {
+                        CreateCell(x, y);
+                    }
+                    else
+                    {
+                        _aliveGrid[x, y] = value;
+                    }
+                }
+            }
         }
     }
 }
